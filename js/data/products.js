9b33b70807409f15ab80fe293d189349d6c1dbc4
YanaEgorova <yanaegorova69.3@gmail.com>
2021-08-15 23:00:54 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,346 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Microfiber Pillowcases',
+        price: 5.95,
+        image: './img/img1.png',
+        description: [
+            {
+                text: '100% Polyester',
+            },
+            {
+                text: 'Includes two 20 x 30 inch standard-size pillowcases',
+            },
+            {
+                text: 'Polyester microfiber offers strength and exceptional softness',
+            },
+            {
+                text: 'Easy to care for: machine wash warm, no bleach, tumble dry low',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.jpg'],
+    },
+    {
+        id: 'product-2',
+        name: '24-Pocket Over-the-Door Hanging Medium-Size Shoe Organizer',
+        price: 19.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Store 12 pairs of shoes or 24 pairs of sandals without taking up valuable floor space.',
+            },
+            {
+                text: 'Mesh fabric pockets allows your shoes to breathe.',
+            },
+            {
+                text: 'Pockets are 8.8" tall x 4.5" wide, total dimensions are 64.3" tall x 18.2" wide.',
+            },
+            {
+                text: 'Includes 3 metal hooks for use over residential doors.',
+            },
+            {
+                text: 'No assembly required.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Power Strip Surge Protector',
+        price: 24.99,
+        discount: 14.99,
+        image: './img/img3.png',
+        description: [
+            {
+                title: 'Multi-Outlet Surge Protector Power Strip',
+                text: 'Power everything on your desk with a single compact surge-protected extension cord. This reputable power cord offers 12 AC outlets with surge protector for charging your computer, laptop, phone, camera, and more. One charging station for a clutter-free desk.',
+            },
+            {
+                title: 'Power Bar with Phone Line Protection',
+                text: 'Featuring a 1-in-2-out RJ11 telephone or fax protection to ensure open and continuous phone line connections. It also provides a coaxial cable protection to safeguard cable box and satellite connections. The damage-resistant housing protects circuits from fire, impact, and rust.',
+            },
+            {
+                title: 'Safeguard Your Devices and Appliances',
+                text: 'The electric strip has a 3,940-Joule energy rating providing a reliable power extension cord to protect printers, broadband modems, or home theater systems. It secures your electronic devices from overload, short circuit, power spikes, lightning strikes, or fluctuations.',
+            },
+            {
+                title: 'Compact, Heavy Duty, and Space-Saving',
+                text: 'The widely spaced outlets give ample area to fit big plugs and charger bricks. Its slender design makes it ideal for home office, workstations, and game rooms. The durable cord is 8 ft / 2.4 meters long with a 3-prong angle plug convenient for tight spaces.',
+            },
+            {
+                title: 'Reliable Product and Service',
+                text: 'Purchase with confidence as it is backed by a limited lifetime warranty and protected by a dollar 200,000 Connected Equipment Warranty. Check out the full specs: 6,000 volts maximum spike voltage, 15A AC, 125V, and 1875W. Each outlet has a safety slide lock to close unused sockets safeguarding you and your kids.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Camping Chair with Built-in 4 Can Cooler',
+        price: 35.32,
+        discount: 24.65,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Polyester.',
+            },
+            {
+                text: 'Built-in cooler: Keeps up to 4 cans cold and easily accessible.',
+            },
+            {
+                text: 'Hours of comfort: Thanks to fully cushioned seat and back.',
+            },
+            {
+                text: 'Convenient features: Side pockets, mesh cup holder, adjustable arm heights.',
+            },
+            {
+                text: 'Dimensions: 24 inches wide; 18.1 inches high.',
+            },
+            {
+                text: 'Built to last: Strong steel frame supports up to 325 pounds.',
+            },
+            {
+                text: 'Carry bag: Included.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Neoprene Coated Dumbbell Hand Weight Set',
+        price: 39.99,
+        discount: 39.97,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Set of six cast iron dumbbells in three weight categories with storage stand - 3 Pairs: 20 Pounds Total (2-Lb, 3-Lb, 5-Lb).',
+            },
+            {
+                text: 'Neoprene-coated for durability, safety, and a non-slip grip.',
+            },
+            {
+                text: 'Printed weight number on each end cap and color-coded for quick identification.',
+            },
+            {
+                text: 'Hexagonal shape prevents dumbbells from rolling away.',
+            },
+            {
+                text: 'Easy-to-assemble weight stand included.',
+            },
+            {
+                text: 'Ideal for resistance training.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png', './img/img5-4.png', './img/img5-5.png']
+    },
+    {
+        id: 'product-6',
+        name: 'Ergonomic Foam Pet Bed',
+        price: 43.98,
+        discount: 55.97,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Pet bed with an ergonomic foam interior for cushioning comfort.',
+            },
+            {
+                text: 'Provides joint support and helps alleviates pressure points for more restful sleep.',
+            },
+            {
+                text: 'Soft swirl plush top; polyester canvas sides; oxford fabric bottom offers added strength.',
+            },
+            {
+                text: 'For indoor use. Removable cover is machine washable for easy cleaning. Wash separately in cool water with mild detergent; do not use bleach; do not tumble dry; line dry; do not dry clean or iron; dry completely before use; reshape if needed.',
+            },
+            {
+                text: 'Recommended for large sized breeds.',
+            },
+            {
+                text: 'Measured 36 x 27.5 x 3.5 inches.',
+            },
+        ],
+        images: ['./img/img6-1.png', './img/img6-2.jpg', './img/img6-3.jpg'],
+    },
+    {
+        id: 'product-7',
+        name: '58-Piece Screwdriver Set',
+        price: 49.95,
+        discount: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                text: '58-piece magnetic screwdriver set for easily tightening or loosening screws; includes precision-slotted screwdrivers.',
+            },
+            {
+                text: 'Ideal for anything from repairing wrist watches and jewelry to fixing electronics like laptops, tablets, and phones or anything with micro screws.',
+            },
+            {
+                text: 'Made of durable chromium vanadium steel with chromium plating for corrosion resistance; heavy-duty magnetic black oxide non-slip tips with a blackening treatment for rust resistance and to ensure a secure grip in fasteners.',
+            },
+            {
+                text: 'Non-slip soft handles made of PP+TPR material for strength and a comfortable, secure grip; blow-molded case included for secure storage, easy organizing, and convenient portability.',
+            },
+            {
+                text: 'Measures 12 by 9.8 by 2.8 inches.',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png', './img/img7-3.png', './img/img7-4.png'],
+    },
+    {
+        id: 'product-8',
+        name: 'Desk Drawer Security Safe',
+        price: 69.99,
+        discount: 69.94,
+        image: './img/img8.png',
+        description: [
+            {
+                text: 'Safe with a slim size fits conveniently inside of a drawer.',
+            },
+            {
+                text: 'Programmable electronic keypad for easy operation; emergency back-up key included; small handle on door for quick access.',
+            },
+            {
+                text: '2 live-door bolts and pry-resistant concealed hinges for superior security.',
+            },
+            {
+                text: 'Pre-drilled mounting holes and hardware included for mounting the safe to a drawer.',
+            },
+            {
+                text: 'Product Dimensions: Exterior measures 11.8 x 8.6 x 4.4 inches (LxWxH); interior measures 11.7 x 8.5 x 1.7 inches (LxWxH).',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png', './img/img8-3.png', './img/img8-4.png'],
+    },
+    {
+        id: 'product-9',
+        name: 'Table Easel',
+        price: 79.99,
+        discount: 79.93,
+        image: './img/img9.png',
+        description: [
+            {
+                text: 'Useful for painting or display; table-top construction is extremely stable and compact, a convenient and reliable space-saver; adjustable via a brass wingnut, this easel has a full height of 40"',
+            },
+            {
+                text: 'Oiled, sanded beech wood construction.',
+            },
+            {
+                text: 'Accommodates canvases up to 23. 5"',
+            },
+            {
+                text: 'Weighs 3. 5 lbs.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'Full Motion Articulating TV Mount',
+        price: 84.41,
+        discount: 95.50,
+        image: './img/img10.png',
+        description: [
+            {
+                text: 'Articulating TV wall mount accommodates 37- to 80-inch TVs (up to 132 pounds) for enhanced home TV-viewing experience.',
+            },
+            {
+                text: 'Tilts 6-10 degrees up and down; 180 degree right-left swivel capability; articulating design extends 2.8”–26” from the wall (7.1cm – 66cm) inches from the wall.',
+            },
+            {
+                text: 'Made of heavy-duty steel for reliable strength.',
+            },
+            {
+                text: 'Universal compatibility to fit the following VESA TV mounting hole patterns: 200X100mm, 200X150mm, 200X200mm, 300X200mm, 300X300mm, 400X200mm, 400X300mm, 400X400mm, 600X400mm.',
+            },
+            {
+                text: 'Fits stud 16” apart.',
+            },
+            {
+                text: 'Includes: manual, template, hardware kit, 6-ft HDMI Cable.',
+            },
+            {
+                text: 'Easy do-it-yourself instructions; pre-set drill hole template and mounting hardware included.',
+            },
+            {
+                text: 'Minimal design that hides bulky hardware, wires, and cords from general view; ideal for home entertainment, business or corporate settings, schools, restaurants, bars, and more.',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.png', './img/img10-3.png', './img/img10-4.jpg'],
+    },
+    {
+        id: 'product-11',
+        name: 'Cycling Helmet',
+        price: 89.99,
+        discount: 109.91,
+        image: './img/img11.png',
+        description: [
+            {
+                text: 'Utilizes the Multi-Directional Impact Protection System to redirect energy in a crash.',
+            },
+            {
+                text: 'Get the right fit, straight out of the box.',
+            },
+            {
+                text: '22 vents.',
+            },
+            {
+                text: 'In-mold construction.',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.png'],
+    },
+    {
+        id: 'product-12',
+        name: 'Wireless Bluetooth Earbuds Headphones',
+        price: 109.00,
+        discount: 110.89,
+        image: './img/img12.png',
+        description: [
+            {
+                text: 'Bluetooth 5.0 earbuds for listening to music and taking calls hands-free.',
+            },
+            {
+                text: 'Inductive magnet for charging ear buds; charges quickly on-the-go without needing to be plugged in.',
+            },
+            {
+                text: 'Micro USB charging dock doubles as carrying case and can independently charge ear buds when not plugged in.',
+            },
+            {
+                text: '55 mAh lithium-ion battery provides up to 7 hours of operation at 30%-40% volume.',
+            },
+            {
+                text: 'Easy pairing with a variety of bluetooth devices; quality sound and built-in microphone.',
+            },
+            {
+                text: 'Bluetooth connectivity radius of 45 feet.',
+            },
+        ],
+        images: ['./img/img12-1.png', './img/img12-2.png', './img/img12-3.png'],
+    },
+    {
+        id: 'product-13',
+        name: 'Steel Garden Cart',
+        price: 119.00,
+        discount: 120.88,
+        image: './img/img13.png',
+        description: [
+            {
+                title: 'Tough and durable',
+                text: '34-inch long x 18-inch wide steel mesh bed and 10-inch pneumatic tires.',
+            },
+            {
+                title: 'Easy to use',
+                text: 'Innovative frame design allows for quick and easy assembly while offering smooth steering, sturdy rigidity, and ample ground clearance.',
+            },
+            {
+                title: 'Versatile hauling',
+                text: 'Side panels are removable to accommodate larger loads.',
+            },
+            {
+                title: 'Comfortable pulling',
+                text: 'Padded pull handle makes it easy to pull up to 400 pounds.',
+            },
+        ],
+        images: ['./img/img13-1.png', './img/img13-2.png', './img/img13-3.png', './img/img13-4.png'],
+    },
+]
